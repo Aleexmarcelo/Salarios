4097b2d944f815ac0e845e07a7386b6f7d0326bc
Aleexmarcelo <jojoanete@gmail.com>
2021-10-27 19:21:22 -0300

    Adicionar arquivos de projeto.

---
+++ Salarios/Funcionarios.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Salarios
+{
+    class Funcionarios
+    {
+        //Encapsulamento
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Salario { get; set; }
+
+        //Metodo de incrementar o salario
+        public void incrementarSalario(double porcentagem)
+        {
+            Salario = Salario + (Salario*( porcentagem/100));
+        }
+
+        internal static void Add(Funcionarios funcionarios)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static object Find(Func<object, bool> p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
